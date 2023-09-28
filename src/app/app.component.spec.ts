16b8abc937204af5a51e6e7869d1f21a413f3432
Omar Esparza <omar.esparza@tangelo.nl>
2023-09-28 20:33:57 +0200

    feat():Hiding Add button on other route than default

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-crash'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-crash');
   });
 
   it('should render title', () => {
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {UiService} from "../../services/ui.service";
 import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -11,12 +12,15 @@ export class HeaderComponent {
   title = 'Task Tracker';
   showAddTask: boolean;
   subscription:Subscription;
-
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService,private router:Router) {
     this.subscription=this.uiService.onToggle().subscribe((value)=>this.showAddTask=value);
   }
   toggleAddTask():void{
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route:string){
+    return this.router.url===route;
   }
 
 }
