b510fec7846f72efabfce7f694ff64876f1f7b4e
Omar Esparza <omar.esparza@tangelo.nl>
2023-09-28 19:14:23 +0200

    feat(): Adding delete task feature

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,17 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+import {Task} from "../../Task";
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input("task") task:Task;
+  @Output() onDeleteTask:EventEmitter<Task> = new EventEmitter<Task>();
+  faTimes = faTimes;
+  onDelete(task:Task){
+    this.onDeleteTask.emit(task);
+  }
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Task} from "../Task";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getTasks(): Observable<Task[]> {
+
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(id: number): Observable<Task> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<Task>(url);
+  }
+}
